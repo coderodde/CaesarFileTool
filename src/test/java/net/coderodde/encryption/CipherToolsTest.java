cc7fe5b37ced0527b74bbfd36f4a4fa3dfa6a01d
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-02-29 20:01:10 +0200

    Initial commit.

---
+++ src/test/java/net/coderodde/encryption/CipherToolsTest.java
@@ -0,0 +1,46 @@
+package net.coderodde.encryption;
+
+import java.util.Arrays;
+import java.util.Random;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CipherToolsTest {
+
+    private static final int ITERATIONS = 100;
+    private static final int MAXIMUM_LENGTH = 1000;
+
+    @Test
+    public void testEncryptionDecryption() {
+        long seed = System.nanoTime();
+        Random random = new Random(seed);
+        System.out.println("Seed = " + seed);
+
+        for (int iteration = 0; iteration < ITERATIONS; iteration++) {
+            int cipherKey = random.nextInt();
+
+            if (cipherKey == 0) {
+                cipherKey = 1;
+            }
+
+            byte[] before = new byte[random.nextInt(MAXIMUM_LENGTH + 1)];
+            random.nextBytes(before);
+
+            byte[] encrypted = CipherTools.encrypt(before, cipherKey);
+            byte[] after = CipherTools.decrypt(encrypted,  cipherKey);
+
+            assertTrue(Arrays.equals(before, after));
+            assertFalse(Arrays.equals(before, encrypted));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEncryptionThrowsOnZeroCipher() {
+        CipherTools.encrypt(new byte[2], 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDecryptionThrowsOnZeroCipher() {
+        CipherTools.decrypt(new byte[2], 0);
+    }
+}
