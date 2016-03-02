71cffac91477bac3c39304137c0495281e2b93ad
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-03-02 11:12:46 +0200

    Refactored a little bit.

--- src/main/java/net/coderodde/ciphertool/App.java
+++ src/main/java/net/coderodde/ciphertool/App.java
@@ -21,7 +21,7 @@ import net.coderodde.file.FileTools;
  * @version 1.6 (Feb 29, 2016) 
  */
 public class App {
-    
+
     private static final String HELP_MESSAGE = 
      "Usage: java -jar File.jar [-e | -d <key> FILE1 [FILE2 [...]]]\n" +
      "Where       -e   encrypt the files.\n" +
@@ -29,65 +29,52 @@ public class App {
      "         <key>   the key in decimal; " + 
                       "use prefix \"0x\" for hexadecimal.\n" +
      "If you omit all arguments a GUI is started instead.";
-    
+
     private boolean graphicalInterfaceRequested;
     private boolean printHelpMessage;
     private Mode mode;
     private int key;
     private String[] args;
-    
+
     public void processArguments(String[] args) {
         this.args = args.clone();
-        
+
         if (args.length == 0) {
             graphicalInterfaceRequested = true;
             return;
         }
-        
+
         if (args.length < 3) {
             printHelpMessage = true;
             return;
         }
-        
+
         switch (args[0]) {
             case "-e":
                 mode = Mode.ENCRYPTING;
                 break;
-                
+
             case "-d":
                 mode = Mode.DECRYPTING;
                 break;
         }
-        
+
         if (mode == null) {
             printHelpMessage = true;
             return;
         }
-        
-        if (args[1].length() >= 2 && 
-           (args[1].startsWith("0x") || args[1].startsWith("0X"))) {
-            String keyString = args[1].substring(2).trim().toLowerCase();
-            
-            try {
-                key = Integer.parseInt(keyString, 16);
-            } catch (NumberFormatException ex) {
-                printHelpMessage = true;
-                return;
-            }
-        } else {
-            try {
-                key = Integer.parseInt(args[1]);
-            } catch (NumberFormatException ex) {
-                printHelpMessage = true;
-                return;
-            }
+
+        try {
+            this.key = parseKey(args[1]);
+        } catch (NumberFormatException ex) {
+            printHelpMessage = true;
         }
     }
-    
+
     public boolean helpMessageRequested() {
         return printHelpMessage;
     }
-    
+
     public void exec() {
         if (graphicalInterfaceRequested) {
             SwingUtilities.invokeLater(() -> { execAsGUI(); });
@@ -95,25 +82,25 @@ public class App {
             execAsCommandLine();
         }
     }
-    
+
     private void execAsGUI() {
         JFrame frame = new JFrame("File cipher tool");
         JButton buttonEncrypt = new JButton("Encrypt");
         JButton buttonDecrypt = new JButton("Decrypt");
-        
+
         buttonEncrypt.setPreferredSize(new Dimension(200, 40));
         buttonDecrypt.setPreferredSize(new Dimension(200, 40));
-        
+
         buttonEncrypt.addActionListener(
                 new MyActionListener(frame, Mode.ENCRYPTING));
-        
+
         buttonDecrypt.addActionListener(
                 new MyActionListener(frame, Mode.DECRYPTING));
-        
+
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(buttonEncrypt, BorderLayout.NORTH);
         frame.getContentPane().add(buttonDecrypt, BorderLayout.SOUTH);
-        
+
         frame.pack();
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -121,27 +108,38 @@ public class App {
         frame.setVisible(true);
     }
     
+    static int parseKey(String keyString) throws NumberFormatException {
+        keyString = keyString.trim().toLowerCase();
+        
+        if (keyString.length() >= 2
+                && (keyString.startsWith("0x")
+                || keyString.startsWith("0X"))) {
+            return Integer.parseInt(keyString.substring(2), 16);
+        } 
+        
+        return Integer.parseInt(keyString);
+    }
+    
     private static void moveToCenter(JFrame frame) {
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation((screen.width  - frame.getWidth())  >> 1,
                           (screen.height - frame.getHeight()) >> 1);
     } 
-    
+
     private void execAsCommandLine() {
         List<File> fileList = getFileList(args);
-        
+
         switch (mode) {
             case ENCRYPTING:
                 encryptAll(fileList, key);
                 break;
-                
+
             case DECRYPTING:
                 decryptAll(fileList, key);
                 break;
         }
-        
     }
-    
+
     static void encryptAll(List<File> fileList, int key) {
         fileList.stream().forEach((File file) -> {
             try {
@@ -153,7 +151,7 @@ public class App {
             }
         });
     }
-    
+
     static void decryptAll(List<File> fileList, int key) {
         fileList.stream().forEach((File file) -> {
             try {
@@ -165,35 +163,34 @@ public class App {
             }
         });
     }
-    
+
     private List<File> getFileList(String[] args) {
         // We subtract 2 in order to omit the fist two arguments in 'args' that
         // are the switch and the key.
         List<File> fileList = new ArrayList<>(args.length - 2);
-        
+
         for (int i = 2; i < args.length; ++i) {
             fileList.add(new File(args[i]));
         }
-        
+
         return fileList;
     }
-    
+
     private String getHelpMessage() {
         return HELP_MESSAGE;
     }
-    
+
     public static void main(String[] args) throws IOException {
         try {
             App app = new App();
             app.processArguments(args);
-            
+
             if (app.helpMessageRequested()) {
                 System.out.println(app.getHelpMessage());
                 System.exit(0);
             }
-            
+
             app.exec();
-            
         } catch (RuntimeException ex) {
             System.err.println("ERROR: " + ex.getMessage());
             System.exit(1);
